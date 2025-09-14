de203f54a5f05e14648c70231b421eafa8dd5270
miriskalt <56968935+miriskalt@users.noreply.github.com>
2025-09-15 00:23:48 +0200

    Update streamlit_app.py
    added title, subtitle and text to DS demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
+import pandas as pd
 import streamlit as st
 
-st.title("🎈 A Data Science Demo Project")
+st.set_page_config(page_title="DS Example", page_icon="🎈")
+
+
+@st.cache_data
+def load_data():
+    # df = pd.read_csv("data/movies_genres_summary.csv")
+    return df
+
+
+df = load_data()
+
+
+st.title("🎈 A Data Science Demo Project using Streamlit")
+st.title("This is a sample project using human data")
+
 st.write(
-    "This is the demo for an interactive website."
+    "This is the demo for an interactive website.
+    This is a sample project on human data showcasing the features readiness, stress and overall scores defined by FitBit. Enjoy."
 )
+
+st.subheader("Linegraph of single feature over entire timespan")
+st.write("
+         The following interactive linegraph visualizes the selected features over the entire timespan of the dataset
+         ")
+
+st.pills("")
+
