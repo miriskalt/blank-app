4d6d9c1c7fa488c30855dffd13a5ba9afc7e2f4c
miriskalt <56968935+miriskalt@users.noreply.github.com>
2025-09-15 00:51:00 +0200

    Update streamlit_app.py
    testing second line graph

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ st.line_chart(data=df, y=options[plot_feature], x="Date")
 
 ###### Section 2 all features over entire timespan
 st.subheader("Linegraph of all features over entire timespan")
-st.line_chart(y=df[list(options.values())], x="Date")
+st.line_chart(data=df, y=df[list(options.values())], x="Date")
